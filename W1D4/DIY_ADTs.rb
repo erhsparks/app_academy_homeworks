beaa6a9593b03b32e98fd33f29793174ed2d0b2c
Elizabeth Sparks <erhsparks@gmail.com>
2016-09-16 06:55:14 -0700

    add Map class, implemented using only arrays

--- W1D4/DIY_ADTs.rb
+++ W1D4/DIY_ADTs.rb
@@ -36,6 +36,46 @@ end
 
 
 class Map
+  def initialize
+    @map = []
+  end
 
+  def assign(key, value)
+    found_key = false
 
+    @map.map! do |pair|
+      if pair[0] == key
+        found_key = true
+
+        [pair[0], value]
+      else
+        pair
+      end
+    end
+    @map << [key, value] unless found_key
+
+    [key, value]
+  end
+
+  def lookup(key)
+    @map.select { |pair| return pair[1] if pair[0] == key }
+
+    nil
+  end
+
+  def remove(key)
+    found_key = nil
+
+    @map.delete_if do |pair|
+      found_key = pair if pair[0] == key
+      
+      pair[0] == key
+    end
+
+    found_key
+  end
+
+  def show
+    @map
+  end
 end
