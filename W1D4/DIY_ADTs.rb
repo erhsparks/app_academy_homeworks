8b6cc9c740f297575377cb2a287899e2e8de0c7a
Elizabeth Sparks <erhsparks@gmail.com>
2016-09-16 06:14:24 -0700

    add Stack class to DIY ADTs

---
+++ W1D4/DIY_ADTs.rb
@@ -0,0 +1,29 @@
+class Stack
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    @stack << el
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack
+  end
+end
+
+class Queue
+
+
+
+end
+
+
+class Map
+
+
+end
