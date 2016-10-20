0afeef912e4c5acf4ed93c4acb0b4304718b1860
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-20 00:09:49 -0700

    w6d3 add weather in NYC, not London

--- w6d3/first_ajax_skeleton/js/index.js
+++ w6d3/first_ajax_skeleton/js/index.js
@@ -3,7 +3,8 @@ console.log("Hello from the JavaScript console!");
 // Your AJAX request here
 $.ajax({
     type: 'GET',
-    url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b",
+    // url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
     success(data) {
         console.log("We have your weather!")
         console.log(data);
